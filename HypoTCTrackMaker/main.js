25169d9490799afd8af65ce899646cf730472f5a
Hypercane <Reno-Rex@users.noreply.github.com>
2022-08-23 02:20:18 -0400

    Update main.js

--- HypoTCTrackMaker/main.js
+++ HypoTCTrackMaker/main.js
@@ -689,7 +689,7 @@ var HypoTrack = (function(){
         if(key === 'd')
             categoryToPlace = 0;
         else if(key === 's')
-            categoryToPlace = 1; // test
+            categoryToPlace = 1;
         else if(key === '0')
             categoryToPlace = 2;
         else if(key === '1')
